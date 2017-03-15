5e621b6edb508e9bf88e0af9a64bf12fd698660e
Daniel Alvarez <a3dany@gmail.com>
2017-03-15 00:40:07 -0400

    Starting point

--- README.md
+++ README.md
@@ -2,3 +2,33 @@
 
 Punto de partida del ejemplo de Firebase Realtime Database en objetos Producto.
 
+![device-2017-03-15-002344](https://cloud.githubusercontent.com/assets/1444991/23933790/490a206a-0917-11e7-9e95-22f849c97812.png)
+
+## Paso 0
+
+    compile 'com.google.firebase:firebase-database:10.0.1'
+
+## Paso 1
+
+    private DatabaseReference databaseRef;
+    
+## Paso 2    
+    
+    databaseRef = FirebaseDatabase.getInstance().getReference();
+            DatabaseReference productosRef = databaseRef.child("productos");
+    
+            productosRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    productosAdapter.clear();
+                    for (DataSnapshot msgSnapshot : snapshot.getChildren()) {
+                        Producto msg = msgSnapshot.getValue(Producto.class);
+                        productosAdapter.add(msg);
+                    }
+                }
+    
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    Toast.makeText(getApplicationContext(), R.string.cancel_process, Toast.LENGTH_SHORT).show();
+                }
+            });
--- app/src/main/java/tech/alvarez/products/MainActivity.java
+++ app/src/main/java/tech/alvarez/products/MainActivity.java
@@ -6,17 +6,13 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference databaseRef;
+    // Paso 1
 
     private ListView productsListView;
     private ProductosAdapter productosAdapter;
@@ -35,24 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Firebase
 
-        databaseRef = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference productosRef = databaseRef.child("productos");
-
-        productosRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                productosAdapter.clear();
-                for (DataSnapshot msgSnapshot : snapshot.getChildren()) {
-                    Producto msg = msgSnapshot.getValue(Producto.class);
-                    productosAdapter.add(msg);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                Toast.makeText(getApplicationContext(), R.string.cancel_process, Toast.LENGTH_SHORT).show();
-            }
-        });
+        // Paso 2
     }
 
     public void adicionarProducto(View view) {
